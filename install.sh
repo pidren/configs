0b45f5c5411db27aa0b148876d521cb910701e1b
ren <yhrenlee@gmail.com>
2019-06-18 20:08:39 -0700

    config: fixing install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Convenience script to copy config files to user's homedir
 
-tools=$(find . -type d -d 1 -not -path "*/\.*" -print | cut -c3-)
+tools=$(find . -depth -maxdepth 1 -type d -not -path '*/\.*' -print | cut -c3-)
 for t in $tools
 do
    echo "Installing $t configs"
