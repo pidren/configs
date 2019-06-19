fda2324493bb138b21adf437de3f092b85187698
ren <yhrenlee@gmail.com>
2019-06-18 22:46:45 -0700

    fixing setup script

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ args=$1
 
 if [[ $1 == "all" ]]; then
   echo "Install godeb"
-  (cd /tmp && wget https://godeb.s3.amazonaws.com/godeb-amd64.tar.gz && tar -xzf godeb-amd64.tar.gz)
-  (cd /tmp && ./godeb install $(./godeb list | head -n 1 ))
+  ( cd /tmp && wget https://godeb.s3.amazonaws.com/godeb-amd64.tar.gz && tar -xzf godeb-amd64.tar.gz )
+  goversion=$( ./godeb list | head -n 1 )
+  echo "Installing go version $goversion"
+  ( cd /tmp && ./godeb install $goversion )
 fi
